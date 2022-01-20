278f0540af684c1339d844ed25f078252a373642
AdnanYK <adnan.ykassab@gmail.com>
2022-01-20 20:28:41 +0000

    modified logic of breedFetcher to display errors

--- breedFetcher.js
+++ breedFetcher.js
@@ -6,18 +6,31 @@ const URL = 'https://api.thecatapi.com/v1/breeds/search?q=' + breed;
 
 
 request(URL, (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
+
   
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  // console.log('body:', body.length);
-  const result = JSON.parse(body)
-  if (result.length === 0) {
-    console.log('invalid search word');
-  } else {
-    for (let i in result) {
-      console.log(`result #${i} name:`, result[i].name); // Print the HTML for the Google homepage.
-      console.log('description:', result[i].description); // Print the HTML for the Google homepage.
+
+  if (error === null) {
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    const result = JSON.parse(body)
+    if (result.length === 0) {
+      console.log('invalid search word');
+    } else {
+      for (let i in result) {
+        if (result[i].name !== undefined) {
+          console.log(`result #${i} name:`, result[i].name); // Print the HTML for the Google homepage.
+          console.log('description:', result[i].description); // Print the HTML for the Google homepage.
+        }
+      }
     }
+
+  } else {
+    console.log('error:', error); // Print the error if one occurred
   }
-});
   
+});
+
+
+
+// const breedDescription = fetchBreedDescription('Siberian', (error, description) => {
+
+// });
